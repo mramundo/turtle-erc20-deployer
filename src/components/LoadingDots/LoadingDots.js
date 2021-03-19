1ff99e87a845f6fa203fa9273be05ef0fe186a03
Mirco Ramundo <mramundo@users.noreply.github.com>
2021-03-19 21:24:47 +0100

    Removed @keyframes classes

--- src/components/LoadingDots/LoadingDots.js
+++ src/components/LoadingDots/LoadingDots.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 
 const LoadingDots = () => {
-    return <span className="loading-dots" />
+    const durantion = 250
+    const pattern = useMemo(() => ['.', '..', '...'], [])
+
+    const [text, setText] = useState(pattern[0])
+    const [textIndex, setTextIndex] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setText(pattern[textIndex])
+            setTextIndex(textIndex < pattern.length - 1 ? textIndex + 1 : 0)
+        }, durantion)
+
+        return () => clearInterval(interval)
+    }, [pattern, textIndex])
+
+    return <span className="loading-dots">{text}</span>
 }
 
 export default LoadingDots
--- src/components/LoadingLine/LoadingLine.js
+++ src/components/LoadingLine/LoadingLine.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 
 const LoadingLine = () => {
-    return <span className="loading-line" />
+    const durantion = 50
+    const pattern = useMemo(() => ['-', '\\', '|', '/'], [])
+
+    const [text, setText] = useState(pattern[0])
+    const [textIndex, setTextIndex] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setText(pattern[textIndex])
+            setTextIndex(textIndex < pattern.length - 1 ? textIndex + 1 : 0)
+        }, durantion)
+
+        return () => clearInterval(interval)
+    }, [pattern, textIndex])
+
+    return <span className="loading-line">{text}</span>
 }
 
 export default LoadingLine
